c18cb05214460ca7904f7f075ddaa3345637a0f8
Administrator <Administrator@MSDN-SPECIAL>
2019-02-19 16:46:00 +0900

    [이다은，유지수] ｊｄ＿ｉｎｖｅｓｔ　프로젝트　초기화 －　짝코팅　프로젝트　생성

---
+++ jd_invest/src/main/java/com/bank/invest/jd/JdInvestApplication.java
@@ -0,0 +1,13 @@
+package com.bank.invest.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdInvestApplication.class, args);
+	}
+
+}
