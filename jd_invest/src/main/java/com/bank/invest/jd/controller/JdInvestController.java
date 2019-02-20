7533448593c518e50b51e9d86ddbdba137686017
Administrator <Administrator@MSDN-SPECIAL>
2019-02-20 14:06:28 +0900

    [이다은, 유지수] login시 session 처리와 logout 처리 완성 - session 값이 없을 경우 index.html에서 login 버튼, - session 값이 있을 경우 로그아웃 버튼과 로그인 정보 출력

--- jd_invest/src/main/java/com/bank/invest/jd/controller/JdInvestController.java
+++ jd_invest/src/main/java/com/bank/invest/jd/controller/JdInvestController.java
@@ -1,5 +1,7 @@
 package com.bank.invest.jd.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,16 +17,30 @@ public class JdInvestController {
 	@Autowired
 	private JdInvestService jdInvestService;
 	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
 	
 	@PostMapping("/login")
-	public String login(Member member, Model model) {
+	public String login(Member member, Model model, HttpSession session) {
 		Member resultMember = jdInvestService.login(member);
 		System.out.println("resultMember -> " + resultMember);
+		session.setAttribute("memberId", member.getMemberId());
 		model.addAttribute("member", resultMember);
 		return "index";
 	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		System.out.println("session 초기화 완료");
+		return "redirect:/index";
+	}
+	
 }
